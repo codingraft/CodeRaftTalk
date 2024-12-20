a26655b19fe102b79c8d33607f8ef3fbb4f3a318
Vaibhavg987 <78644114+Vaibhavg987@users.noreply.github.com>
2024-12-21 00:22:33 +0530

    error resolved

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,8 +33,8 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  })
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {

9fb191c728d64700ed3284cd2d9a9f39306f3c52
Vaibhavg987 <78644114+Vaibhavg987@users.noreply.github.com>
2024-12-21 00:30:33 +0530

    error resolved2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
 import cors from "cors";
-import {app, server} from "./lib/socket.js";
+
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-
+dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
-dotenv.config();
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
@@ -29,8 +28,8 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
@@ -38,6 +37,6 @@ if(process.env.NODE_ENV === "production"){
 }
 
 server.listen(PORT, () => {
-  console.log("server started on port" + " " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });

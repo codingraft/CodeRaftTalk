1465b1c47f5c61201e16abd1500c10e053bef515
Vaibhavg987 <78644114+Vaibhavg987@users.noreply.github.com>
2024-12-21 13:47:11 +0530

    vercel upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,12 @@ import Settings from "./pages/Settings";
 import Navbar from "./components/Navbar";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader } from "lucide-react";
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
-  const {theme} = useThemeStore()
+  const { theme } = useThemeStore();
 
   console.log({ onlineUsers });
 
@@ -34,20 +34,27 @@ const App = () => {
     <div data-theme={theme}>
       <Navbar />
       <Routes>
-        <Route path="/" element={authUser ? <Home /> : <Navigate to="/login"/>} />
-        <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to="/"/>}  />
-        <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/"/>} />
-        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login"/>} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <Profile /> : <Navigate to="/login" />}
+        />
         <Route path="/settings" element={<Settings />} />
       </Routes>
 
-      <Toaster
-  position="top-center"
-  reverseOrder={false}
-/>
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
-
-    
   );
 };
 export default App;

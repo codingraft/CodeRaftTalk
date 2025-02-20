85771e73656299b7abab4ce0377baef08b60bb00
codingraft <gvaibhav864@gmail.com>
2025-02-21 01:28:02 +0530

    hdsjfhsd

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: `${process.env.FRONTEND_URL}/`,
+    origin: [`${process.env.FRONTEND_URL}`],
     credentials: true,
   })
 );
